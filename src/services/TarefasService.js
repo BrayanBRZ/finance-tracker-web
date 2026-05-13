a23424291775c586fb2e95292668307b24816028
Brayan Barros <brayanbarrosdm@gmail.com>
2026-05-12 23:18:04 -0300

    reset repository

--- src/services/TarefasService.js
+++
@@ -1,9 +0,0 @@
-const KEY = "lista-tarefas";
-
-export const getTarefas = () => {
-    return JSON.parse(localStorage.getItem(KEY)) || [];
-}
-
-export const salvarTarefas = (lista) => {
-    localStorage.setItem(KEY, JSON.stringify(lista));
-}
