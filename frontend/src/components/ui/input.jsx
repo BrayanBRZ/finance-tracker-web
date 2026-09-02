b2dc09f9ac16c7c6d636a3e4cf19461451bc3867
Brayan Barros <brayanbarrosdm@gmail.com>
2026-09-02 18:23:40 -0300

    fix(campos): padroniza sombra em campos

--- frontend/src/components/ui/input.jsx
+++ frontend/src/components/ui/input.jsx
@@ -1,19 +1,16 @@
 import { cn } from '@/lib/utils'
 
-function Input({
-  className,
-  type,
-  ...props
-}) {
+function Input({ className, type, ...props }) {
   return (
     <input
       type={type}
       data-slot="input"
       className={cn(
-        'flex h-10 w-full min-w-0 rounded-(--radius) border border-input bg-transparent px-3 py-2 text-base text-foreground shadow-sm outline-none transition-colors placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-1 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40',
-        className
+        'border-input text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 disabled:bg-input/50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 flex h-10 w-full min-w-0 rounded-(--radius) border bg-transparent px-3 py-2 text-base shadow-xs transition-colors outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:ring-1 md:text-sm',
+        className,
       )}
-      {...props} />
+      {...props}
+    />
   )
 }
 
